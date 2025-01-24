64eb33140ec2d821aa18e4f2a1fe994f7b4d9db0
asmfc <michel081192@hotmail.fr>
2025-01-25 00:50:21 +0100

    ✨ change pictures caroussel

--- src/components/pages/accueil/sections/Caroussel.jsx
+++ src/components/pages/accueil/sections/Caroussel.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styled from "styled-components";
 
@@ -20,7 +20,7 @@ const Carousel = () => {
       <CarouselContainer>
         <Slider {...settings}>
           {/* Liste des images */}
-          {["caroussel1.webp","caroussel2.webp", "caroussel3.webp", "caroussel4.webp", "caroussel5.webp", "caroussel6.webp"].map((img, index) => (
+          {["caroussel1.jpeg", "caroussel2.jpeg", "caroussel3.jpeg", "caroussel4.jpeg", "caroussel5.jpeg", "caroussel6.jpeg"].map((img, index) => (
             <div key={index} style={{ position: "relative" }}>
               <StyledImage
                 src={`/caroussel/${img}`}
@@ -34,38 +34,33 @@ const Carousel = () => {
   );
 };
 
-// Styled-components pour les images et le conteneur
+
 const CarouselContainer = styled.div`
   width: 100%;
   margin: 0 auto;
-  overflow: hidden; /* Assure que rien ne déborde */
+  overflow: hidden;
 `;
 
 const StyledImage = styled.img`
   width: 100%;
   height: auto;
-  object-fit: cover; /* Permet à l'image de couvrir le conteneur sans être déformée */
-
-  /* Utilisation de media queries pour ajuster les styles sur différents écrans */
+  object-fit: cover;
+  object-position: top;
 
   @media (max-width: 768px) {
-    height: 300px; /* Hauteur spécifique de 200px sur mobile */
-    object-position: center; /* Centrer l'image sur mobile */
+    height: 300px;
   }
 
   @media (min-width: 769px) and (max-width: 1024px) {
-    height: 500px; /* Hauteur spécifique de 500px pour les tablettes */
-    object-position: center; /* Centrer l'image sur tablette */
+    height: 500px;
   }
 
   @media (min-width: 1025px) and (max-width: 1600px) {
-    height: 600px; /* Hauteur spécifique de 800px entre 1025px et 1600px */
-    object-position: center; /* Centrer l'image */
+    height: 600px;
   }
 
   @media (min-width: 1601px) {
-    height: 1000px; /* Hauteur sur ordinateurs de bureau (1000px) */
-    object-position: center; /* Centrer l'image sur desktop */
+    height: 1000px;
   }
 `;
 
