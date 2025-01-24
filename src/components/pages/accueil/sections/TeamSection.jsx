2e57a37434973e2304cd3da403bbf9a73b005d7a
asmfc <michel081192@hotmail.fr>
2025-01-25 00:15:16 +0100

    ✨ Add members pictures

--- src/components/pages/accueil/sections/TeamSection.jsx
+++ src/components/pages/accueil/sections/TeamSection.jsx
@@ -3,9 +3,9 @@ import styled from "styled-components";
 
 const TeamSection = () => {
   const teamMembers = [
-    { name: "Noémie", role: "Experte amincissement & regard" },
-    { name: "Elsa", role: "Esthéticienne polyvalente" },
-    { name: "Maité", role: "Experte sourcils" },
+    { name: "Noémie", role: "Experte amincissement & regard", image: "/team/member1.jpeg" },
+    { name: "Elsa", role: "Esthéticienne polyvalente", image: "/team/member2.jpeg" },
+    { name: "Maité", role: "Experte sourcils", image: "/team/member3.jpeg" },
   ];
 
   return (
@@ -14,7 +14,7 @@ const TeamSection = () => {
       <TeamGrid>
         {teamMembers.map((member, index) => (
           <MemberCard key={index}>
-            <Image src={'/team/personne1.jpeg'} alt={member.name} />
+            <Image src={member.image} alt={member.name} />
             <Info>
               <MemberName>{member.name}</MemberName>
               <Role>{member.role}</Role>
@@ -29,40 +29,34 @@ const TeamSection = () => {
 export default TeamSection;
 
 const Section = styled.section`
-  padding: 10rem 5rem; /* Padding par défaut sur les grands écrans */
+  padding: 10rem 5rem; 
   text-align: center;
   width: 100%;
-  box-shadow: inset 0 0 30px rgba(0, 0, 0, 0.05); /* Effet doux pour le fond */
+  box-shadow: inset 0 0 30px rgba(0, 0, 0, 0.05);
   display: flex;
   flex-direction: column;
-  justify-content: center; /* Centre le contenu verticalement */
-  
-  /* Hauteur en desktop */
-  min-height: 650px; /* Hauteur minimale */
-  height: 100%; /* Garantit que la section prend tout l'espace vertical disponible */
-  
+  justify-content: center;
+  min-height: 650px;
+  height: 100%;
   @media (max-width: 2000px) {
-    padding: 8rem 3rem; /* Réduit le padding sur des écrans plus petits que 2000px */
+    padding: 8rem 3rem;
   }
-  
   @media (max-width: 1024px) {
-    padding: 5rem 2rem; /* Réduction du padding sur tablette */
-    min-height: auto; /* Hauteur dynamique basée sur le contenu */
+    padding: 5rem 2rem;
+    min-height: auto;
   }
-
   @media (max-width: 768px) {
-    padding: 3rem 1rem; /* Réduction du padding sur mobile */
+    padding: 3rem 1rem;
   }
 `;
 
 const Heading = styled.h2`
   font-size: 2rem;
-  color: ${({ theme }) => theme.colors.primary || "#2d3b45"}; /* Couleur du titre */
+  color: ${({ theme }) => theme.colors.primary || "#2d3b45"};
   margin-bottom: 6rem;
   font-weight: bold;
   text-transform: uppercase;
   line-height: 1.3;
-
   @media (max-width: 768px) {
     font-size: 1.5rem;
     margin-bottom: 3rem;
@@ -71,16 +65,14 @@ const Heading = styled.h2`
 
 const TeamGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr); /* Trois colonnes sur grand écran */
+  grid-template-columns: repeat(3, 1fr);
   gap: 2rem;
-
   @media (max-width: 1024px) {
-    grid-template-columns: 1fr 1fr; /* Deux colonnes sur tablette */
+    grid-template-columns: 1fr 1fr;
     gap: 1.5rem;
   }
-
   @media (max-width: 768px) {
-    grid-template-columns: 1fr; /* Une seule colonne sur mobile */
+    grid-template-columns: 1fr;
     gap: 1rem;
   }
 `;
@@ -91,30 +83,24 @@ const MemberCard = styled.div`
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   overflow: hidden;
   transition: transform 0.3s ease-in-out;
-  
-  /* Adapter la hauteur en fonction du mode */
-  height: auto; /* Hauteur automatique sur tous les écrans */
-
+  height: auto;
   &:hover {
     transform: scale(1.05);
   }
-
-  /* Réduction de la hauteur sur mobile */
   @media (max-width: 768px) {
-    height: 300px; /* Réduction de la hauteur sur mobile */
+    height: 800px;
   }
 `;
 
 const Image = styled.img`
   width: 100%;
-  height: 60%; /* Augmente la taille de l'image en desktop */
+  height: 70%;
   object-fit: cover;
 `;
 
 const Info = styled.div`
-  padding: 1rem;
   text-align: center;
-  height: 40%; /* Réduit la taille de la description */
+  height: 30%;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -127,7 +113,6 @@ const MemberName = styled.h3`
 `;
 
 const Role = styled.p`
-  font-size: 0.9rem; /* Réduit la taille de la description */
+  font-size: 0.9rem;
   color: ${({ theme }) => theme.colors.secondary || "#777"};
 `;
-
